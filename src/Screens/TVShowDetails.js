3b031602ccefddd02006de2f283c3f6563608e26
kavithachunduri <105738289+kavithachunduri@users.noreply.github.com>
2022-08-13 12:59:41 -0400

    Update TVShowDetails.js

---
+++ src/Screens/TVShowDetails.js
@@ -0,0 +1,36 @@
+import './App.scss';
+import Home from './Screens/Home';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import {Route,Routes} from 'react-router-dom';
+import Movies from './Screens/Movies';
+import TVShows from './Screens/TVShows';
+import NoMatch from './Screens/NoMatch';
+import TVShowDetails from './Screens/TVShowDetails';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+
+      <Routes>
+
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Movies" element={<Movies/>}/>
+        <Route path="/Movies/:id" element={<Movies/>}/>
+        <Route path="/TVShows" element={<TVShows/>}/>
+        <Route path="/TVShows/:tvshow_id" element={<TVShowDetails/>}/>
+        <Route path="/*" element={<NoMatch/>}/>
+      </Routes>
+
+
+      <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
